924e3946f65be0701d1be5bbe77c58e079595912
ltischuk <laura.tischuk@accesso.com>
2015-11-17 21:00:22 -0500

    first commit

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('rosesAndHerbs')
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.slides = [{imgUrl:'../assets/images/gift-shed.JPG'}];
+
+  });
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('rosesAndHerbs')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
